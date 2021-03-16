d7d895b866995f949dd9d3ed463a8055fd2bc815
Dawid-Cimoch <dawidcc99@gmail.com>
2021-03-16 17:23:45 +0100

    task 2 modify
    changing pliki.cpp to struct type

--- zadanie2-pliki/Dawid_Cimoch_pliki.cpp
+++ zadanie2-pliki/Dawid_Cimoch_pliki.cpp
@@ -1,78 +1,163 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
 
 using namespace std;
-//Funkcja ifSki czy string "text", kończy się na string "text_end"
-//rfind sprawdza czy "text" kończy się w tym samym miejscu co zaczyna się "text_end"
-bool ifSki(const string text, const string text_end)
+
+struct osoba
 {
-    return text.rfind(text_end)==(text.size()-text_end.size());
+    string imie;
+    string nazwisko;
+    int wiek;
+    float skutecznosc;
+    osoba *next;
+    osoba();
+};
+
+osoba::osoba()
+{
+    next=0;
+}
+
+struct list
+{
+    osoba *pierwsza;
+    void add_from_file(string nazwa);
+    void show_list();
+    float max_skutecznosc();
+    bool nazwisko_ski(string nazwisko, string end);
+    void wybierz_osoby();
+    void save_to_file(string nazwa);
+    list();
+};
+
+list::list()
+{
+    pierwsza=0;
+}
+
+void list::add_from_file(string nazwa)
+{
+    fstream first_file(nazwa); //plik do odczytu
+    while(!first_file.eof())
+    {
+        osoba *nowa= new osoba;
+
+        string imie, nazwisko;
+        int wiek;
+        float skutecznosc;
+
+        first_file >> imie >> nazwisko >>wiek >> skutecznosc;
+
+        nowa->imie=imie;
+        nowa->nazwisko=nazwisko;
+        nowa->wiek=wiek;
+        nowa->skutecznosc=skutecznosc;
+
+        if(pierwsza==0) //czy jest to pierwszy element listy
+        {
+            pierwsza = nowa;
+        }
+        else
+        {
+            osoba *temp=pierwsza;
+
+            while(temp->next)
+            {
+                temp = temp->next;
+            }
+            temp->next = nowa;
+            nowa->next = 0;
+        }
+    }
+    first_file.close();
+}
+
+void list::show_list()
+{
+    osoba *temp = pierwsza;
+
+    while(temp)
+    {
+        cout << temp->imie << " " << temp->nazwisko << " " << temp->wiek << " " << temp->skutecznosc << endl;
+        temp = temp->next;
+    }
+}
+
+float list::max_skutecznosc()
+{
+    osoba *temp = pierwsza;
+
+    float max_eff = 0;
+
+    while(temp)
+    {
+        if(temp->skutecznosc > max_eff)
+        {
+            max_eff = temp->skutecznosc;
+        }
+        temp = temp->next;
+    }
+    return max_eff;
+}
+
+bool list::nazwisko_ski(string nazwa, string end)
+{
+    return nazwa.rfind(end)==(nazwa.length()-end.length());
+}
+
+void list::save_to_file(string nazwa)
+{
+    ofstream secound_file(nazwa);
+
+    float max_eff = max_skutecznosc();
+
+    osoba *temp = pierwsza;
+
+    bool rekord = false;
+
+    while(temp)
+    {
+        if(temp->imie.length()>3 && nazwisko_ski(temp->nazwisko, "ski")==true && temp->skutecznosc==max_eff)
+        {
+            for(int i=1; i<temp->imie.length()-3; i++)
+            {
+                temp->imie[i]='*';
+            }
+            for(int i=1; i<temp->nazwisko.length()-3; i++)
+            {
+                temp->nazwisko[i]='*';
+            }
+            secound_file << temp->imie << " " << temp->nazwisko << " " << temp->wiek << endl;
+            rekord = true;
+        }
+        temp = temp->next;
+    }
+    if(rekord==false)
+    {
+        secound_file << "\n" << endl;
+    }
+    secound_file.close();
 }
 
 int main()
 {
-    cout << "Podaj nazwe pliku, ktory chcesz wczytac" << endl;
-    string file_name;
-    cin>>file_name;
-    string full_file_name=file_name+".txt";
-    fstream first_file(full_file_name); //plik do odczytu
-    float max_eff=0;
-    bool if_existe=false;
-    if(first_file)
-    {
-        string imie;
-        string nazwisko;
-        int wiek;
-        float skutecznosc;
-        while(!first_file.eof())
-        {
-            first_file>>imie;
-            first_file>>nazwisko;
-            first_file>>wiek;
-            first_file>>skutecznosc;
-            if(skutecznosc>max_eff)
-            {
-                max_eff=skutecznosc;
-            }
-        }
-        first_file.seekg(0, ios::beg); //ustawiam pozycje początkową odczytu danych od pierwszego znaku, a przesuniecie pozycji jest wyrazone wzgledem poczatku pliku 
-        string full_secound_file_name = file_name + ".max" + ".txt";
-        ofstream secound_file(full_secound_file_name); //plik do zapisu
-        while(!first_file.eof())
-        {
-            first_file>>imie;
-            first_file>>nazwisko;
-            first_file>>wiek;
-            first_file>>skutecznosc;
-            if(imie.length()>3 && ifSki(nazwisko, "ski")==1 && skutecznosc==max_eff)
-            {
-                int len_imie = imie.length();
-                int len_nazwisko = nazwisko.length();
-                //anonimizujemy dane
-                for(int i=1; i<len_imie-3;i++)
-                {
-                    imie[i]='*';
-                }
-                for(int i=1; i<len_nazwisko-3; i++)
-                {
-                    nazwisko[i]='*';
-                }
-                secound_file<<imie<<" "<<nazwisko<<" "<<wiek<<endl;
-                if_existe=true;
-            }
-        }
-        if(if_existe==false)
-        {
-            secound_file<<"\n";
-        }
-        first_file.close();
-        secound_file.close();
-    }
-    else
-    {
-        cout<<"Nie mozna otworzyc pliku"<<endl;
-    }
+    list *data = new list;
+    string name;
+    string full_name;
+    string max_name;
+
+    cout << "Wpisz nazwe pliku: " << endl;
+    cin >> name;
+    
+    full_name=name+".txt";
+    max_name= name+".max.txt";
+
+    data->add_from_file(full_name);
+    data->save_to_file(max_name);
+
+    delete(data);
     system("pause");
     return 0;
 }
